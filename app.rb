cb643ec45e4608d9303e26d807f4178a8ed63cd0
Jason Crockett <JesusisLord33333@outlook.com>
2019-11-26 17:36:56 +0000

    corrected erb tags.

--- app.rb
+++ app.rb
@@ -36,5 +36,5 @@ end
 
 get '/' do
   a = session['omniauth'].to_s
-  erb "<html><head></head><body><%= user_logged_in? ? session['user_id'].to_s : 'user logged out' =%> <br> <%= a =%><a href='/auth/twitter'>login</a></body></html>"
+  erb "<html><head></head><body><%= user_logged_in? ? session['user_id'].to_s : 'user logged out' %> <br> <%= a %><a href='/auth/twitter'>login</a></body></html>"
 end
