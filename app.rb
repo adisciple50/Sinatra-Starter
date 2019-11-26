35055cac7f101d5908405c52f6b40590342ffd20
Jason Crockett <JesusisLord33333@outlook.com>
2019-11-26 16:20:25 +0000

    completed basic twitter auth

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
-require 'sinatra'
-require 'oauth2'
+# http://recipes.sinatrarb.com/p/middleware/twitter_authentication_with_omniauth
 
+require 'sinatra'
+# require 'oauth2'
 require 'omniauth-twitter'
 
 configure do
@@ -9,4 +10,28 @@ configure do
   use OmniAuth::Builder do
     provider :twitter, ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET']
   end
+end
+
+helpers do
+  # check if user is authenticated.
+  def user
+     not session[:user_id].nil?
+  end
+end
+
+
+before do
+#   do not redirect to twitter if the path starts with auth.
+  unless request.path_info =~ /\A\/[a][u][t]h\// # copyright free equivenet regex
+    redirect('/auth/twitter') unless user
+  end
+end
+
+get '/auth/twitter/callback' do
+  session['user_id'] = env['omniauth.auth']['uid']
+  redirect '/'
+end
+
+get '/' do
+  erb "<%= session['user_id'] =%>"
 end
