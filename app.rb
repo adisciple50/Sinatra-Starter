16ae5da33c9c15d86a1962459d659de12240ea9c
Jason Crockett <JesusisLord33333@outlook.com>
2019-11-26 17:28:40 +0000

    added to string conversions.

--- app.rb
+++ app.rb
@@ -35,5 +35,5 @@ get '/auth/twitter/callback' do
 end
 
 get '/' do
-  erb "<html><head></head><body><%= user_logged_in? ? session['user_id'] : 'user logged out' =%> <br> <%= session['omniauth'] =%><a href='/auth/twitter'>login</a></body></html>"
+  erb "<html><head></head><body><%= user_logged_in? ? session['user_id'].to_s : 'user logged out' =%> <br> <%= session['omniauth'].to_s =%><a href='/auth/twitter'>login</a></body></html>"
 end
