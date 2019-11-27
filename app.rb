5914f0a0307b2e7b5450deffcde79a9aafdea6e8
Jason Crockett <JesusisLord33333@outlook.com>
2019-11-27 17:43:36 +0000

    added facebook to the login procedure.

--- app.rb
+++ app.rb
@@ -3,20 +3,27 @@
 require 'sinatra'
 # require 'oauth2'
 require 'omniauth-twitter'
-
+require 'omniauth-facebook'
 configure do
   enable :sessions
 
   use OmniAuth::Builder do
     provider :twitter, 'CSSWZDYS8JmmS8u44BMds727x', 'G6etXygEAMk5EghpWnPE1i0eYgrkR6YKaOEtOW4WKRQyuLiC1E'
+    # https://github.com/mkdynamic/omniauth-facebook
+    provider :facebook, '2587047431403245' , 'bc33a5fe08047af8d471805b3602b75d',
+             callback_url: 'https://sinatra-starter.herokuapp.com/auth/facebook/callback'
   end
 end
 
 helpers do
   # check if user is authenticated.
-  def user_logged_in?
+  def twitter_logged_in?
       # if a session is not found return false. return true otherwise.
-      return session[:user_id].nil? ? false : true
+      return session[:twitter_id].nil? ? false : true
+  end
+  def twitter_logged_in?
+      # if a session is not found return false. return true otherwise.
+      return session[:facebook_id].nil? ? false : true
   end
 end
 
@@ -29,7 +36,13 @@ end
 # end
 
 get '/auth/twitter/callback' do
-  session['user_id'] = env['omniauth.auth']['uid']
+  session['twitter_id'] = env['omniauth.auth']['uid']
+  # session['omniauth'] = env['omniauth.auth'] # the session is too big with this!
+  redirect '/'
+end
+
+get '/auth/facebook/callback' do
+  session['facebook_id'] = env['omniauth.auth']['uid']
   # session['omniauth'] = env['omniauth.auth'] # the session is too big with this!
   redirect '/'
 end
